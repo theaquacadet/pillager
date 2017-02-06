c87a68ad126c9556f8b725c6e6a54413230d69ff
Jake R <jakebrasmussen@gmail.com>
2017-02-06 11:22:34 -0500

    added -c flag to wget

--- pillager.sh
+++ pillager.sh
@@ -8,6 +8,6 @@ echo -n "Link to pillage: "
 
 read LINK
 
-wget -r -np -e robots=off --reject index.html,index.html* ${LINK} -P $SAVEPATH
+wget -r -np -e robots=off -c --reject index.html,index.html* ${LINK} -P $SAVEPATH
 
 echo "Finished. Yar."
